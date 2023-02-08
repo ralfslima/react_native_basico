b64da8869dc1d4ec013499fdf5e600033c53b764
Ralf Schweder de Lima <ralf.lima@PROWAY.TREINA>
2023-02-07 21:11:50 -0300

    React Native Avançado - Aula 02

--- agenda/telas/Inicio.js
+++ agenda/telas/Inicio.js
@@ -1,23 +1,46 @@
 // Importação de módulos
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 
 // Componente
 export default function Inicio(){
+
+    // useState
+    const [formularioLoginAtivo, setFormularioLoginAtivo] = useState(true);
+    const [nomeCriarConta, setNomeCriarConta] = useState('');
+    const [senhaCriarConta, setSenhaCriarConta] = useState('');
+    const [vetor, setVetor] = useState([]);
 
     // Retornar estrutura
     return(
         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
             
+            {formularioLoginAtivo === true ?
+
             <View style={estilos.formulario}>
                 <Image source={require('./imagens/login.png')} style={estilos.imagem} />
 
                 <TextInput style={estilos.input} placeholder="Nome de usuário"              right={<TextInput.Icon icon="account" />} />
                 <TextInput style={estilos.input} placeholder="Senha" secureTextEntry={true} right={<TextInput.Icon icon="lock" />}    />
-                <Button    style={estilos.botao} mode="contained">Logar</Button>
+                <Button    style={estilos.botaoLogar} mode="contained">Logar</Button>
+                <Button    style={estilos.botaoCriarConta} mode="contained" onPress={() => {setFormularioLoginAtivo(false)}}>Criar conta</Button>
             </View>
 
-        </View>
+            : 
+
+            <View style={estilos.formulario}>
+                <Image source={require('./imagens/usuario.png')} style={estilos.imagem} />
+
+                <TextInput onChangeText={setNomeCriarConta}  style={estilos.input} placeholder="Nome de usuário"              right={<TextInput.Icon icon="account" />} />
+                <TextInput onChangeText={setSenhaCriarConta} style={estilos.input} placeholder="Senha" secureTextEntry={true} right={<TextInput.Icon icon="lock" />}    />
+                <Button    onPress={criarConta} style={estilos.botaoCriarConta} mode="contained">Criar conta</Button>
+                <Button    style={estilos.botaoCriarConta} mode="contained" onPress={() =>{setFormularioLoginAtivo(true)}} >Voltar</Button>
+                
+            </View>
+
+            }
+            </View>
     );
 
 }
@@ -25,8 +48,8 @@ export default function Inicio(){
 // CSS
 const estilos = StyleSheet.create({
     imagem:{
-        width: 200,
-        height: 200,
+        width: 150,
+        height: 150,
         marginBottom: 30
     },
     formulario:{
@@ -36,7 +59,12 @@ const estilos = StyleSheet.create({
         marginBottom: 10,
         width: 250
     },
-    botao:{
-        width: 100
+    botaoLogar:{
+        width: 250,
+        marginTop: 20
+    },
+    botaoCriarConta:{
+        marginTop: 10,
+        width: 250
     }
 });
